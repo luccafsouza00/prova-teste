5fd7cebfb1a22cc00b948aac81a6ea4b192f5b73
luccafsouza00 <luccafsouza00@gmail.com>
2021-02-26 21:47:28 -0300

    subindo testes

---
+++ automacao.shoestock/src/test/java/teste/Test.java
@@ -0,0 +1,36 @@
+package teste;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import elementos.Elementos;
+import metodo.Metodos;
+
+public class Test {
+	
+	Metodos metodo = new Metodos();
+	Elementos elemento = new Elementos();
+	
+
+
+	@Given("^que esteja \"([^\"]*)\"$")
+	public void que_esteja(String site) throws Throwable {
+		metodo.abrirSite(site);
+	}
+
+	@When("^realize uma busca de produto$")
+	public void realize_uma_busca_de_produto() throws Throwable {
+		metodo.digitar(elemento.getBuscar(), "bota");
+		metodo.submit(elemento.getBuscar());
+		metodo.clickar(elemento.getProduto());
+		metodo.clickar(elemento.getTamanho());
+		metodo.clickar(elemento.getComprar());
+	}
+
+	@Then("^valido os produtos na tela de pagamento$")
+	public void valido_os_produtos_na_tela_de_pagamento() throws Throwable {
+		metodo.validarProduto(elemento.getValidaProduto(), "Bota Couro Shoestock Cano Curto Travel Feminina");
+		metodo.fecharNavegador();
+  
+	}
+}
